382a9f392c529c86ca6a1449b2f0ba6182c9b3df
dinkrr <srjsingh38.ss@gmail.com>
2022-06-23 18:38:06 +0530

    Adding files

---
+++ Injecto/InjectoContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Injecto
+{
+    public class InjectoContainer
+    {
+        private readonly List<Type> _dependencies = new List<Type>();
+        
+        public void AddDependency<T>()
+        {
+            _dependencies.Add(typeof(T));
+        }
+
+        public Type GetDependency(Type type)
+        {
+            return _dependencies.First(x => x.Name == type.Name);
+        }
+    }
+}
---
+++ Injecto/InjectoResolver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Injecto
+{
+    public class InjectoResolver
+    {
+        InjectoContainer _container;
+
+        public InjectoResolver(InjectoContainer container)
+        {
+            _container = container;
+        }
+
+        public T GetService<T>() => (T) GetService(typeof(T));
+
+        private object GetService(Type type)
+        {
+            var dependency = _container.GetDependency(type);
+
+            var constructor = dependency.GetConstructors().Single();
+            var parameters = constructor.GetParameters().ToArray();
+
+            if (parameters.Length > 0)
+            {
+                object[] parameterImplementations = GetParameterImplementations(parameters);
+                return Activator.CreateInstance(dependency, parameterImplementations);
+            }
+
+            return Activator.CreateInstance(dependency);
+        }
+
+        private object[] GetParameterImplementations(System.Reflection.ParameterInfo[] parameters)
+        {
+            var parameterImplementations = new object[parameters.Length];
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                parameterImplementations[i] = GetService(parameters[i].ParameterType);
+            }
+
+            return parameterImplementations;
+        }
+    }
+}
